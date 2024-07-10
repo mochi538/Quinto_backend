df37db438e8282758642fbff488667379e8c441d
Onixdrew <andrewmosquera1811@gmail.com>
2024-07-10 16:08:09 -0500

    Tercer commit

--- Ejercicios/01PrimerTaller.js
+++ Ejercicios/01PrimerTaller.js
@@ -1,61 +1,72 @@
-class Prestamo{
-    constructor(valorId, valornombre, valorValor, valorcuotas, valorfecha,valorInteres){
-      this.Id = valorId
-      this.nombre = valornombre
-      this.valor = valorValor
-      this.conIntereses = []   
-      this.fecha = valorfecha
-      this.cuotas = valorcuotas
-      this.valorInteres = valorInteres
-    }
-   CalcularValorTotalPrestamo(){
+class Prestamo {
+  constructor(valorValor, valorcuotas, valorfecha, valorInteres) {
+    this.valor = valorValor;
+    this.conIntereses = [];
+    this.fecha = valorfecha;
+    this.cuotas = valorcuotas;
+    this.valorInteres = valorInteres;
+  }
+  infouser(Id, nombre) {
+    this.Id = Id;
+    this.nombre = nombre;
+  }
+
+  CalcularValorTotalPrestamo() {
     let valor = parseFloat(this.valor);
-    let valInteres = parseFloat(this.valorInteres)
-    let cuotas =parseFloat(this.cuotas)
-    
-    let total = valor +(cuotas*(valor * valInteres)) 
-    this.conIntereses = (total);
-    console.log(`El valor total del prestamo es: $${this.conIntereses}`)
+    let valInteres = parseFloat(this.valorInteres);
+    let cuotas = parseFloat(this.cuotas);
 
-   }
+    let total = valor + cuotas * (valor * valInteres);
+    this.conIntereses = total;
+    console.log(`El valor total del prestamo es: $${this.conIntereses}`);
+  }
 
-   ValorPagarInteres(){
-    let valor = parseFloat(this.valor)
-    let cuotas = parseFloat(this.cuotas)
-    let total = valor/cuotas
-    console.log(`Valor de cada cuota sin interés $${total}`)
-   }
-   PagarCuota(valorPagar){
-    let valor = parseFloat(this.valor)
-    let valInteres = parseFloat(this.valorInteres)
-    let cuotas= parseFloat(this.cuotas)
-    if(valor > 0){
-        let total = (valor +(cuotas*(valor * valInteres))) - valorPagar
-        console.log(`Aun debe esta cantidad: $${total}`)
-    } else{
-        console.log(`No debe nada :)`)
-    }
-   }
-   Refinanciar(numeroCuotas){
-    this.cuotas = numeroCuotas
+  ValorPagarInteres() {
     let valor = parseFloat(this.valor);
-    let valInteres = parseFloat(this.valorInteres)
-    let cuotas =parseFloat(this.cuotas)
-    
-    let total = valor +(cuotas*(valor * valInteres)) 
-    console.log(`Nuevo Total a pagar $${total}`)
-
-   }
-   toString(){
-    console.log(`Infromación del prestamo: nombre:${this.nombre}, Valor:${this.valor}, Valor con:${this.nombre}`)
-   }
+    let cuotas = parseFloat(this.cuotas);
+    let total = valor / cuotas;
+    console.log(`Valor de cada cuota sin interés $${total}`);
+  }
+  PagarCuota(valorPagar) {
+    let valor = parseFloat(this.valor);
+    let valInteres = parseFloat(this.valorInteres);
+    let cuotas = parseFloat(this.cuotas);
+    if (valor > 0) {
+      let total = valor + cuotas * (valor * valInteres) - valorPagar;
+      console.log(`Aun debe esta cantidad: $${total}`);
+    } else {
+      console.log(`No debe nada :)`);
+    }
+  }
+  Refinanciar(numeroCuotas) {
+    this.cuotas = numeroCuotas;
+    let valor = parseFloat(this.valor);
+    let valInteres = parseFloat(this.valorInteres);
+    let cuotas = parseFloat(this.cuotas);
+    let total = valor + cuotas * (valor * valInteres);
+    this.conIntereses = total;
+    console.log(`Nuevo Total a pagar $${this.conIntereses}`);
+  }
+  toString() {
+    console.log(
+      `Infromación del prestamo: 
+      Nombre:${this.nombre}, 
+      Valor del prestamo:${this.valor},
+      Número de cuotas:${this.cuotas}, 
+      Valor del prestamo con Interés:${this.conIntereses},
+      Fecha del prestamo: ${this.fecha},
+      Valor del procentaje del intereses: ${this.valorInteres * 100}% 
+      `
+    );
+  }
 }
-this.Id = valorId
-      this.nombre = valornombre
-      this.valor = valorValor
-      this.conIntereses = []   
-      this.fecha = valorfecha
-      this.cuotas = valorcuotas
-      this.valorInteres = valorInteres
-let instancia = new Prestamo(123, "Luz", "300", 4, 56, 0.05)
-instancia.Refinanciar(5)
+/* this.valor = valorValor; v
+    this.conIntereses = [];
+    this.fecha = valorfecha;
+    this.cuotas = valorcuotas;
+    this.valorInteres = valorInteres; */
+
+let instancia = new Prestamo("300", 4, "09/07/2024", 0.05);
+instancia.infouser("123", "Luz");
+instancia.Refinanciar(8);
+instancia.toString();
---
+++ Ejercicios/02Segundo.js
@@ -0,0 +1,12 @@
+/* Ordenar miembros del hogar por edades de mayor a menor */
+class hogar{
+    constructor(){
+        this.nombre=
+        this.edad=
+        this.rol=
+}
+
+
+    
+
+}
